fe1a06f9b707c51f9c7df048f48b2786c3e1a7ee
Firaol Negash <feranegash604@gmail.com>
2025-05-13 06:48:49 +0300

    Create Domino piling.cpp

---
+++ Domino piling.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <set>
+using namespace std;
+
+int main()
+{
+    int r,c;
+    cin >> r>>c;
+    cout << (r*c)/2 << endl;
+    return 0;
+}
